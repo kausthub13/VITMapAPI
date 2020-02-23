a40e3af9c088f02eb4c0c63ad7b7e16c1e5ba489
Kausthub Kesavadas <kausthub13@gmail.com>
2020-02-23 13:01:20 +0530

    Added redis URL

--- celery_settings.py
+++ celery_settings.py
@@ -3,5 +3,5 @@ import os
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_BROKER_URL = os.environ.get('CLOUDAMQP_URL','pyamqp://guest@localhost//')
 # CELERY_RESULT_BACKEND = 'db+sqlite:///results.db'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL','redis://127.0.0.1:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
